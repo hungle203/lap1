d7871bb55823dd62a6ffa2be17156bc19cfe5b49
hungle203 <leviethungkm2003@gmail.com>
2024-12-26 12:49:27 +0700

    test2

--- obj/Debug/net9.0/lap1.AssemblyInfo.cs
+++ obj/Debug/net9.0/lap1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lap1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31c3dc3e98ec45931e6db2a90edf63ed60cab25f")]
 [assembly: System.Reflection.AssemblyProductAttribute("lap1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lap1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
